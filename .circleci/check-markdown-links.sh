c74bf4b311fa8a6637fd4385a75bf6d90afff3e6
mieubrisse <mieubrisse@gmail.com>
2021-03-13 12:42:24 -0600

    Actually set config

--- .circleci/check-markdown-links.sh
+++ .circleci/check-markdown-links.sh
@@ -5,7 +5,9 @@ root_dirpath="$(dirname "${script_dirpath}")"
 # We can use relative syntax to specify kurtosis-core/kurtosis-libs in URLs because everything gets
 # published to the same docs.kurtosistech.com domain, but when checking Markdown links we need to expand
 # the full domain name
-CONFIG_JSON='{
+config_filepath="$(mktemp)"
+cat << EOF > "${config_filepath}"
+{
     "replacementPatterns": [
         { 
             "pattern": "^./kurtosis-core",
@@ -16,12 +18,14 @@ CONFIG_JSON='{
             "replacement": "https://docs.kurtosistech.com/kurtosis-libs"
         }
     ]
-}'
+}
+EOF
+
 
 # Inspired by https://github.com/open-telemetry/opentelemetry-collector/pull/1156/files/2244e61f4dd0378deffc00d939edf6f800687dcf
 exit_code=0
 for filepath in $(find "${root_dirpath}" -iname '*.md' | sort); do
-    markdown-link-check -qv "${filepath}" || exit_code=1
+    markdown-link-check --config "${config_filepath}" -qv "${filepath}" || exit_code=1
     # Wait to scan files so that we don't overload github with requests which may result in 429 responses
     sleep 2
 done
